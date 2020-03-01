29bee46589095205c0f9cb3e2c1d792133c89a3a
Sal7_one <oo32dash1@gmail.com>
2020-03-01 07:31:29 +0300

    merging

---
+++ sharedhub/lib/ui/newpost.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class newpost extends StatelessWidget {
+  const newpost({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Container(
+        child: Text("data"),
+      ),
+    );
+  }
+}
+
